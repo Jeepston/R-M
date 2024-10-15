0755ccc41532d69814f03a4c65398896aba9f052
Dmitrij Hojkolov <dmitrij.hojkolov@eman.cz>
2024-10-15 18:30:45 +0200

    Initial Commit

---
+++ rm/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  rm
+//
+//  Created by Dmitrij Hojkolov on 15.10.2024.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ rmTests/rmTests.swift
@@ -0,0 +1,17 @@
+//
+//  rmTests.swift
+//  rmTests
+//
+//  Created by Dmitrij Hojkolov on 15.10.2024.
+//
+
+import Testing
+@testable import rm
+
+struct rmTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
